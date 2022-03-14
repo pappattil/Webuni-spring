7bec3e7369a94597308c7908d862de53d3c25b69
Attila <pappattil@gmail,.com>
2022-03-14 14:54:40 +0100

    airport projekt 1. gyakorlat

---
+++ airport/src/main/java/hu/webuni/airport/service/DefaultDiscountService.java
@@ -0,0 +1,19 @@
+package hu.webuni.airport.service;
+
+import java.io.ObjectInputFilter.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import hu.webuni.airport.config.AirportConfigProperties;
+
+@Service
+public class DefaultDiscountService implements DiscountService{
+	@Autowired
+	AirportConfigProperties config;
+	@Override
+	public int getdiscountPercent(int totalPrice) {
+		return config.getDiscount().getDef().getPercent();
+	}
+
+}
