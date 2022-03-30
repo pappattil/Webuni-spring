6c7d95608a1fe8cd2a788b180f809f6b959045f4
attila <attila@debian>
2022-03-30 07:52:24 +0200

    Add Query to Rest API

--- hr/src/main/java/hu/webuni/hr/itatti/web/HrController.java
+++ hr/src/main/java/hu/webuni/hr/itatti/web/HrController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import hu.webuni.hr.itatti.dto.HrDto;
@@ -23,10 +25,10 @@ import hu.webuni.hr.itatti.dto.HrDto;
 public class HrController {
 	private Map<Long, HrDto> employees = new HashMap<>();
 	{
-		employees.put(1L, new HrDto(1L, "Öreg József", "Raktárvezető", 400000, LocalDateTime.of(1993,5,1,13,0)));
-		employees.put(2L, new HrDto(2L, "Farkas Piroska", "Irodavezető", 400000, LocalDateTime.of(2014,1,01,10,0)));
-		employees.put(3L, new HrDto(3L, "Jóska István", "Operátor", 400000, LocalDateTime.of(2019,9,1,10,00)));
-		employees.put(4L, new HrDto(4L, "Fiatal Ödön", "Adminisztrátor", 400000, LocalDateTime.of(2021,1,1,10,0)));
+		employees.put(1L, new HrDto(1L, "Öreg József", "Raktárvezető", 420000, LocalDateTime.of(1993,5,1,13,0)));
+		employees.put(2L, new HrDto(2L, "Farkas Piroska", "Irodavezető", 415000, LocalDateTime.of(2014,1,01,10,0)));
+		employees.put(3L, new HrDto(3L, "Jóska István", "Operátor", 410000, LocalDateTime.of(2019,9,1,10,00)));
+		employees.put(4L, new HrDto(4L, "Fiatal Ödön", "Adminisztrátor", 405000, LocalDateTime.of(2021,1,1,10,0)));
 	
 	}
 	@GetMapping
@@ -40,6 +42,13 @@ public class HrController {
 			return ResponseEntity.ok(employeeDto);
 		return ResponseEntity.notFound().build();
 	}
+	
+	@GetMapping(params = "minSalary")
+	public List<HrDto> getAllMinSalary(@RequestParam int minSalary) {
+
+		return employees.values().stream().filter(e -> e.getSalary() > minSalary).collect(Collectors.toList());
+	}
+	
 	@PostMapping
 	public HrDto createEmployee(@RequestBody HrDto employeedto) {
 		employees.put(employeedto.getId(), employeedto);
--- hr/src/main/java/hu/webuni/hr/itatti/web/HrTLController.java
+++ hr/src/main/java/hu/webuni/hr/itatti/web/HrTLController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hu.webuni.hr.itatti.HrApplication;
 import hu.webuni.hr.itatti.dto.HrDto;
@@ -30,7 +31,7 @@ public class HrTLController {
 //	public String home() {
 //		return "index";
 //	}
-	
+		
 	@GetMapping("/")
 	public String listEmployee(Map<String, Object> model) {
 	model.put("employees", allEmployee);
